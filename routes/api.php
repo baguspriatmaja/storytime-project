8f9ae7552da5e37db95ed65e412cd924e16a2c82
Bagus Priatmaja <priatmaja9bagus@gmail.com>
2025-01-10 23:01:15 +0800

    update-api

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// Route Auth
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout']);
 
 
+// Route User
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/users/{userId}/update', [UserController::class, 'updateProfile']);
     Route::post('/users/{userId}/image', [UserController::class, 'updateProfileImage']);
@@ -34,18 +35,28 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users/auth', [UserController::class, 'getAuthUser']);
 });
 
-
-Route::apiResource('/stories', StoryController::class, ['only' => ['index', 'show']]);
-Route::apiResource('/category', CategoryController::class, ['only' => ['index', 'show']]);
+// Route Story
+Route::get('/stories', [StoryController::class, 'index']);
+Route::get('/stories/{storyId}', [StoryController::class, 'show']);
 Route::get('/stories/category/{categoryId}', [StoryController::class, 'getStoriesByCategory']);
 Route::get('/get/latest-stories', [StoryController::class, 'getLatestStory']);
 Route::get('/get/newest-stories', [StoryController::class, 'getNewestStory']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/stories/add', [StoryController::class, 'store']);
+    Route::post('/stories/{storyId}/update', [StoryController::class, 'update']);
+    Route::delete('/stories/{storyId}/delete', [StoryController::class, 'destroy']);
+});
+
+
+// Route Category
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{categoryId}', [CategoryController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('/stories', StoryController::class, ['except' => ['index', 'show']]);
-    Route::apiResource('/category', CategoryController::class, ['except' => ['index', 'show']]);
-    Route::apiResource('/bookmark', App\Http\Controllers\BookmarkController::class);
+    Route::post('/categories/add', [CategoryController::class, 'store']);
+    Route::put('/categories/{categoryId}/update', [CategoryController::class, 'update']);
+    Route::delete('/categories/{categoryId}/delete', [CategoryController::class, 'destroy']);
 });
 
 
